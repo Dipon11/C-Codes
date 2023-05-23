1ee1d1accb95fe5030dda05e9e9a34fe976100dd
Dipon11 <dipondas1132@gmail.com>
2023-05-23 21:20:55 +0600

    CPP

---
+++ Module 1/Swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+// int my_swap(int *a,int *b)
+// {
+//     int tmp=*a;
+//     *a= *b;
+//     *b=tmp;
+// }
+int main()
+{
+
+    int a, b;
+    cin >> a >> b;
+
+    // my_swap(&a,&b);
+    swap(a, b);
+    cout << a << " " << b << endl;
+    return 0;
+}
---
+++ Module 1/input_output_c++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   std::cout << "Hello World";
+   std::cout<<"\n";
+   int a=100,b=22;
+//    std::cout<<a<<b;
+    cout<<a<<endl<<b<<endl;
+}
