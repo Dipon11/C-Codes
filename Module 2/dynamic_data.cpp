d930c461ba2734268aed6908ac8be611ad87d393
Dipon11 <dipondas1132@gmail.com>
2023-05-24 23:10:36 +0600

    module 2

---
+++ Module 2/dynamic_data.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // int *a = new int;
+    // *a = 10;
+    float *f = new float;
+    *f = 10.22;
+    cout << *f << endl;
+
+    delete f;
+     return 0;
+}
---
+++ Module 2/ternanry.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int x = 10;
+    // if (x%2==0)
+    // {
+    // cout<<"Even";
+    // }
+    // else{
+    //     cout<<"Odd";
+    // }
+
+    //  Using ternanry operations we can solve this problem
+    (x % 2 == 0) ? cout << "even " : cout << "odd";
+
+    return 0;
+}
